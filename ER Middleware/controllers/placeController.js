176ef433bcac261b89be2a0cdce948d6464b2063
AndreRdz7 <A01730716@itesm.mx>
2018-10-28 17:49:53 -0600

    place n price controllers

---
+++ ER Middleware/controllers/placeController.js
@@ -0,0 +1,101 @@
+const Sequelize = require('sequelize');
+const winston = require('winston');
+
+module.exports = function(app){
+    let place = app.models.schema.place;
+
+    let placeController = {
+        index: function(req,res){
+            place.findAll({})
+            .then(function(place){
+                winston.log('Succes at getting all places from the BD');
+                res.status(200).json(place);
+            })
+            .catch(err => {
+                winston.error(err);
+                res.json(err);
+            })
+        },
+        create: function(req,res){
+            place.create({
+                place_type_id: req.body.place_type_id || null,
+                narrative_id: req.body.narrative_id || null,
+                name: req.body.name || null,
+                longitude: req.body.longitude || null,
+                latitude: req.body.latitude || null,
+                description: req.body.description || null
+            })
+            .then(newPlace => {
+                winston.log('Created a new place');
+                res.status(200).json(newPlace);
+            })
+            .catch(err => {
+                winston.error(err);
+                res.json(err);
+            });
+        },
+        read: function(req,res){
+            let place_id = req.params.id;
+            place.findById(req.params.id,{})
+                .then(place => {
+                    if(!place){
+                        return res.status(404).json({
+                            message: 'Place not found'
+                        });
+                    }
+                    res.status(200).json(place);
+                })
+                .catch(err => {
+                    res.json(err);
+                })
+        },
+        update: function(req, res){
+            place.findById(req.params.place_id, {})
+                .then(place =>{
+                    if(!place){
+                        return res.status(404).json({
+                            message: 'Place not found'
+                        });
+                    }
+                    place
+                        .update({ 
+                            place_type_id: req.body.place_type_id || place.place_type_id,
+                            narrative_id: req.body.narrative_id || place.narrative_id,
+                            name: req.body.name || place.name,
+                            longitude: req.body.longitude || place.longitude,
+                            latitude: req.body.latitude || place.latitude,
+                            description: req.body.description || place.description
+                        })
+                        .then(() => res.status(200).json(place))
+                        .catch(err => {
+                            res.status(400).json(err);
+                        })
+                })
+                .catch(err => {
+                    res.json(err);
+                })
+        },
+        delete: function(req,res){
+            place.findById(req.params.id)
+                .then(place => {
+                    if(!place){
+                        return res.status(400).json({
+                            message: 'Place not found'
+                        });
+                    }
+                    return place
+                        .update({
+                            active: false
+                        })
+                        .then(() => res.status(200).json({
+                            message: 'Place image unactive'
+                        }))
+                        .catch(err => {
+                            res.status(400).json(err);
+                        })
+                })
+        }
+    }//place_imageController
+
+    return placeController;
+};
---
+++ ER Middleware/controllers/priceController.js
@@ -0,0 +1,95 @@
+const Sequelize = require('sequelize');
+const winston = require('winston');
+
+module.exports = function(app){
+    let price = app.models.schema.price;
+
+    let priceController = {
+        index: function(req,res){
+            price.findAll({})
+            .then(function(price){
+                winston.log('Succes at getting all prices from the BD');
+                res.status(200).json(price);
+            })
+            .catch(err => {
+                winston.error(err);
+                res.json(err);
+            })
+        },
+        create: function(req,res){
+            price.create({
+                ticket_type_id: req.body.ticket_type_id || null,
+                tour_id: req.body.tour_id || null,
+                amount: req.body.amount || null
+            })
+            .then(newPrice => {
+                winston.log('Created a new price');
+                res.status(200).json(newPrice);
+            })
+            .catch(err => {
+                winston.error(err);
+                res.json(err);
+            });
+        },
+        read: function(req,res){
+            let price_id = req.params.id;
+            price.findById(req.params.id,{})
+                .then(price => {
+                    if(!price){
+                        return res.status(404).json({
+                            message: 'Price not found'
+                        });
+                    }
+                    res.status(200).json(price);
+                })
+                .catch(err => {
+                    res.json(err);
+                })
+        },
+        update: function(req, res){
+            price.findById(req.params.price_id, {})
+                .then(price =>{
+                    if(!price){
+                        return res.status(404).json({
+                            message: 'Price not found'
+                        });
+                    }
+                    price
+                        .update({ 
+                            ticket_type_id: req.body.ticket_type_id || price.ticket_type_id,
+                            tour_id: req.body.tour_id || price.tour_id,
+                            amount: req.body.amount || price.amount
+                        })
+                        .then(() => res.status(200).json(price))
+                        .catch(err => {
+                            res.status(400).json(err);
+                        })
+                })
+                .catch(err => {
+                    res.json(err);
+                })
+        },
+        delete: function(req,res){
+            price.findById(req.params.id)
+                .then(price => {
+                    if(!price){
+                        return res.status(400).json({
+                            message: 'Price not found'
+                        });
+                    }
+                    return price
+                        .update({
+                            active: false
+                        })
+                        .then(() => res.status(200).json({
+                            message: 'Price image unactive'
+                        }))
+                        .catch(err => {
+                            res.status(400).json(err);
+                        })
+                })
+        }
+    }//place_imageController
+
+    return priceController;
+};
