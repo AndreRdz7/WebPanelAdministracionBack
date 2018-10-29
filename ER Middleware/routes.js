0a90f3c8c7b29daf3ae04d19b991859cdaae6660
AndreRdz7 <A01730716@itesm.mx>
2018-10-28 18:50:39 -0600

    controller routes

--- ER Middleware/routes.js
+++ ER Middleware/routes.js
@@ -1,27 +1,31 @@
 const passport = require('passport');
 
 module.exports = function(app){
+    let companyController = app.controllers.companyController;
+    let ticket_typeController = app.controllers.ticket_typeController;
+    let muralController = app.controllers.muralController;
+    let place_typeController = app.controllers.place_typeController;
+    let purchaseController = app.controllers.purchaseController;
+    let priceController = app.controllers.priceController;
     let userController = app.controllers.userController;
+    let placeController = app.controllers.placeController;
+    let ticketController = app.controllers.ticketController;
+    let tourController = app.controllers.tourController;
+    let busController = app.controllers.busController;
     let narrativeController = app.controllers.narrativeController;
-
-    /*
-    //User routes
-    app.route('/api/user')
-        .get(userController.index)
-        .post(userController.create);
-    app.route('/api/user/:id')
-        .get(userController.read)
-        .put(userController.update)
-        .delete(userController.delete);
-    */ 
-    //Narrative routes
-    app.route('/api/narrative')
-        .get(narrativeController.index)
-        .post(narrativeController.create);
-    app.route('/api/narrative/:id')
-        .get(narrativeController.read)
-        .put(narrativeController.update)
-        .delete(narrativeController.delete);
+    let braceletController = app.controllers.braceletController;
+    let tour_scheduleController = app.controllers.tour_scheduleController;
+    let date_intervalController = app.controllers.date_intervalController;
+    let tour_placeController = app.controllers.tour_placeController;
+    let place_imageController = app. controllers.place_imageController;
+    let activated_atController = app.controllers.activated_atController;
+    let tour_stopController = app.controllers.tour_stopController;
+    let stopController = app.controllers.stopController;
+    let scheduleController = app.controllers.scheduleController;
+    let hour_intervalController = app.controllers.hour_intervalController;
+    let imageController = app.controllers.imageController;
+    
+    
     
     //Company routes
     app.route('/api/company')
@@ -59,6 +63,60 @@ module.exports = function(app){
         .put(place_typeController.update)
         .delete(place_typeController.delete);
 
+    //Purchase  routes
+    app.route('/api/purchase')
+        .get(purchaseController.index)
+        .post(purchaseController.create);
+    app.route('/api/purchase/:id')
+        .get(purchaseController.read)
+        .put(purchaseController.update)
+        .delete(purchaseController.delete);
+
+    //Price routes
+    app.route('/api/price')
+        .get(priceController.index)
+        .post(priceController.create);
+    app.route('/api/pri /:id')
+        .get(priceController.read)
+        .put(priceController.update)
+        .delete(priceController.delete);
+
+    //User routes
+    app.route('/api/user')
+        .get(userController.index)
+        .post(userController.create);
+    app.route('/api/user/:id')
+        .get(userController.read)
+        .put(userController.update)
+        .delete(userController.delete);
+    
+    //Place routes
+    app.route('/api/place')
+        .get(placeController.index)
+        .post(placeController.create);
+    app.route('/api/place/:id')
+        .get(placeController.read)
+        .put(placeController.update)
+        .delete(placeController.delete);
+
+    //Ticket routes
+    app.route('/api/ticket')
+        .get(ticketController.index)
+        .post(ticketController.create);
+    app.route('/api/ticket/:id')
+        .get(ticketController.read)
+        .put(ticketController.update)
+        .delete(ticketController.delete);
+
+    //Tour routes
+    app.route('/api/tour')
+        .get(tourController.index)
+        .post(tourController.create);
+    app.route('/api/tour/:id')
+        .get(tourController.read)
+        .put(tourController.update)
+        .delete(tourController.delete);
+
     //Bus routes
     app.route('/api/bus')
         .get(busController.index)
@@ -68,6 +126,15 @@ module.exports = function(app){
         .put(busController.update)
         .delete(busController.delete);
 
+    //Narrative routes
+    app.route('/api/narrative')
+        .get(narrativeController.index)
+        .post(narrativeController.create);
+    app.route('/api/narrative/:id')
+        .get(narrativeController.read)
+        .put(narrativeController.update)
+        .delete(narrativeController.delete);
+
     //Bracelet routes
     app.route('/api/bracelet')
         .get(braceletController.index)
@@ -76,6 +143,24 @@ module.exports = function(app){
         .get(braceletController.read)
         .put(braceletController.update)
         .delete(braceletController.delete);
+
+    //Tour schedule routes
+    app.route('/api/tour_schedule')
+        .get(tour_scheduleController.index)
+        .post(tour_scheduleController.create);
+    app.route('/api/tour_schedule/:id')
+        .get(tour_scheduleController.read)
+        .put(tour_scheduleController.update)
+        .delete(tour_scheduleController.delete);
+
+    //Date interval routes
+    app.route('/api/date_interval')
+        .get(date_intervalController.index)
+        .post(date_intervalController.create);
+    app.route('/api/date:_interval/:id')
+        .get(date_intervalController.read)
+        .put(date_intervalController.update)
+        .delete(date_intervalController.delete);
 
     //Tour place routes
     app.route('/api/tour_place')
@@ -103,7 +188,42 @@ module.exports = function(app){
         .get(activated_atController.read)
         .put(activated_atController.update)
         .delete(activated_atController.delete);
+
+    //Tour stop routes
+    app.route('/api/tour_stop')
+        .get(tour_stopController.index)
+        .post(tour_stopController.create);
+    app.route('/api/tour_Stop/:id')
+        .get(tour_stopController.read)
+        .put(tour_stopController.update)
+        .delete(tour_stopController.delete);
+
+    //Stop routes
+    app.route('/api/stop')
+        .get(stopController.index)
+        .post(stopController.create);
+    app.route('/api/stop/:id')
+        .get(stopController.read)
+        .put(stopController.update)
+        .delete(stopController.delete);
+
+    //Schedule routes
+    app.route('/api/schedule')
+        .get(scheduleController.index)
+        .post(scheduleController.create);
+    app.route('/api/schedule/:id')
+        .get(scheduleController.read)
+        .put(scheduleController.update)
+        .delete(scheduleController.delete);
     
+    //Hour interval routes
+    app.route('/api/hour_interval')
+        .get(hour_intervalController.index)
+        .post(hour_intervalController.create);
+    app.route('/api/hour_interval/:id')
+        .get(hour_intervalController.read)
+        .put(hour_intervalController.update)
+        .delete(hour_intervalController.delete);
 
     //Image routes
     app.route('/api/image')
