bb77e3817a05534d8f79f6bc3caaeb9ff87e9e32
AndreRdz7 <A01730716@itesm.mx>
2018-10-31 10:23:53 -0600

    place type test

---
+++ test/update_place_type.js
@@ -0,0 +1,22 @@
+const supertest = require('supertest');
+const expect = require('chai').expect;
+const schema = require('../validation/place_type_schema');
+const joi = require('joi');
+
+
+let baseURL = supertest("https://reqres.in");
+let place_type = "/api/place_type/1";
+
+describe('PUT Request', () => {
+    let put_resp;
+    it('makes a POST call ', async () => {
+        put_resp = await baseURL.put(place_type)
+            .type('form')
+            .send({
+                "name": "hotel",
+            })
+            .set('Accept', '/application/\json/');
+
+        await (console.log(put_resp.body));
+    });
+});
