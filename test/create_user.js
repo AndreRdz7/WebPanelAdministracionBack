3a034a2d7994d00200ab4c3b60f1dfecce93f72f
AndreRdz7 <A01730716@itesm.mx>
2018-10-31 11:35:34 -0600

    user test

---
+++ test/create_user.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest');
+const expect = require('chai').expect;
+const schema = require('../validation/user_schema');
+const joi = require('joi');
+
+
+let baseURL = supertest("https://reqres.in");
+let user = "/api/user";
+
+describe('POST Request',()=>{
+    let post_resp;
+    it('makes a POST call ',async ()=>{
+        post_resp = await baseURL.post(user)
+        .type('form')
+        .send({
+                "user_type": "admin",
+                "name": "Rayu",
+                "last_name": "Amin",
+                "email": "rayu@er.com",
+                "birthdate": "1/1/1980",
+                "password": "bimbo",
+                "postal_code": "77777",
+                "phone_number": "2222121212"
+        })
+        .set('Accept','/application/\json/');
+
+        await (console.log(post_resp.body));
+    });
+});
