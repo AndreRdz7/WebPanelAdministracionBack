42c419f13a6254b8cbf9cdfebe65aff578173432
AndreRdz7 <A01730716@itesm.mx>
2018-10-31 11:49:35 -0600

    place test

---
+++ test/update_place.js
@@ -0,0 +1,27 @@
+const supertest = require('supertest');
+const expect = require('chai').expect;
+const schema = require('../validation/place_schema');
+const joi = require('joi');
+
+
+let baseURL = supertest("https://reqres.in");
+let place = "/api/place/1";
+
+describe('PUT Request', () => {
+    let put_resp;
+    it('makes a POST call ', async () => {
+        put_resp = await baseURL.put(place)
+            .type('form')
+            .send({
+                "place_type_id": "1",
+                "narrative_id": "1",
+                "name": "catedral",
+                "longitude": "0",
+                "latitude": "0",
+                "description": "la catedral mas extrema de mexico"
+            })
+            .set('Accept', '/application/\json/');
+
+        await (console.log(put_resp.body));
+    });
+});
