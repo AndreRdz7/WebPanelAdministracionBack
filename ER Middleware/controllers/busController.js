a42c58d7800b9b122918eff0a9635ce8213e73c2
AndreRdz7 <A01730716@itesm.mx>
2018-11-06 16:52:20 -0600

    el typo mas mamalon y mamastroso que hemos tenido

--- ER Middleware/controllers/busController.js
+++ ER Middleware/controllers/busController.js
@@ -18,7 +18,7 @@ module.exports = function(app){
         create: function(req,res){
             bus.create({
                 tour_id: req.body.tour_id || null,
-                mural_id: req.params.mural_id || null,
+                mural_id: req.body.mural_id || null,
                 capacity: req.body.capacity || null,
                 sold_tickets: req.body.sold_tickets || null,
                 status: req.body.status || null
